2c8a58bf4a4d0ab0f46c6e25f9d19abe506f70e4
swlho <stephen@swlho.com>
2025-07-04 10:39:05 +0100

    fix: update sql to fix foreign key relationship

--- seeds/music_directory.sql
+++ seeds/music_directory.sql
@@ -4,10 +4,10 @@
 -- database state, and that tests don't interfere with each other.
 
 -- First, we must delete (drop) all our tables
-DROP TABLE IF EXISTS artists;
-DROP SEQUENCE IF EXISTS artists_id_seq;
 DROP TABLE IF EXISTS albums;
 DROP SEQUENCE IF EXISTS albums_id_seq;
+DROP TABLE IF EXISTS artists;
+DROP SEQUENCE IF EXISTS artists_id_seq;
 
 -- Then, we recreate them
 CREATE SEQUENCE IF NOT EXISTS artists_id_seq;
@@ -22,7 +22,10 @@ CREATE TABLE albums (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255),
     release_year INTEGER,
-    artist_id INTEGER
+    artist_id INT,
+    CONSTRAINT fk_artist FOREIGN KEY(artist_id)
+    REFERENCES artists(id)
+    ON DELETE CASCADE
 );
 
 -- Finally, we add any records that are needed for the tests to run
